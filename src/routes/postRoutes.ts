6ede3111acaac77d7d7def11c35ed90ffcdd0c4b
ohad samu <ohadsamu@gmil.com>
2025-01-17 17:35:05 +0200

    feat: swagger

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -6,17 +6,88 @@ import {
   deletePost,
   getPostById,
 } from "../controllers/postController";
+
 const router = express.Router();
 
-router.post("/", createPost);
+// Route to create a new post
+/**
+ * @swagger
+ * /posts:
+ *   post:
+ *     summary: Create a new post
+ *     responses:
+ *       201:
+ *         description: Post created successfully
+ */
+router.post('/', createPost);
 
-router.get("/", getPosts);
+// Route to get all posts
+/**
+ * @swagger
+ * /posts:
+ *   get:
+ *     summary: Get all posts
+ *     responses:
+ *       200:
+ *         description: A list of posts
+ */
+router.get('/', getPosts);
 
-router.get("/:id", getPostById);
+// Route to get a specific post by ID
+/**
+ * @swagger
+ * /posts/{id}:
+ *   get:
+ *     summary: Get a post by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the post
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: The requested post
+ */
+router.get('/:id', getPostById);
 
-router.put("/:id", updatePost);
+// Route to update a specific post by ID
+/**
+ * @swagger
+ * /posts/{id}:
+ *   put:
+ *     summary: Update a post by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the post
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Post updated successfully
+ */
+router.put('/:id', updatePost);
 
-router.delete("/:id", deletePost);
-
+// Route to delete a specific post by ID
+/**
+ * @swagger
+ * /posts/{id}:
+ *   delete:
+ *     summary: Delete a post by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the post
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Post deleted successfully
+ */
+router.delete('/:id', deletePost);
 
 export default router;

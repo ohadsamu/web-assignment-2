61eab0ed1e8af2f45cf84094a06b0dd478be1cc2
ohad samu <ohadsamu@gmil.com>
2025-01-18 15:09:03 +0200

    fix: jwt secret

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -2,16 +2,19 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/user';
 
+const JWT_SECRET = process.env.JWT_SECRET || 'your_secret_key';
+
 export const authenticate = async (req: any, res: any, next: any) => {
   const token = req.header('Authorization')?.replace('Bearer ', '');
   if (!token) return res.status(401).json({ message: 'No token provided' });
 
   try {
     console.log('Token:', token); // Debugging: Log the token
-    const decoded: any = jwt.verify(token, process.env.JWT_SECRET || '');
+    console.log('jwt secret:', JWT_SECRET); // Debugging: Log the token
+    const decoded: any = jwt.verify(token, JWT_SECRET);
     console.log('Decoded:', decoded); // Debugging: Log decoded data
     req.user = await User.findById(decoded.id); 
-    if (!req.user) return res.status(401).json({ message: 'Invalid token' });
+    if (!req.user) res.status(401).json({ message: 'Invalid token' });
     next();
   } catch (err) {
     console.error('Authentication Error:', err); // Debugging
